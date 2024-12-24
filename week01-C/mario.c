e3603b1ef8724af8d848ed8de0d861f55bef983c
Alberto Noguchi <alberto2611@gmail.com>
2024-12-23 23:56:39 -0300

    add mario blocks

--- week01-C/mario.c
+++ week01-C/mario.c
@@ -3,18 +3,9 @@
 
 int main(void)
 {
-  int n;
-  // Prompt user for positive integer
-  do
+  for (int i = 0; i < 4; i++)
   {
-    n = get_int("Size: ");
-  } 
-  while (n < 1);
-  
-  // Print an n-by-n grid of bricks
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    for (int j = 0; j < 4; j++)
     {
       printf("#");
     }
